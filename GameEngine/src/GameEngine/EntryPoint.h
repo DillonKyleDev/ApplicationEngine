45a09b9d9e86ae609e38602b751b6271145aaac4
DillonKyleDev <dillonkyle95@protonmail.com>
2022-11-09 20:56:35 -0700

    Added logging library

--- GameEngine/src/GameEngine/EntryPoint.h
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -2,11 +2,18 @@
 
 #ifdef GE_PLATFORM_WINDOWS
 
+#include "Log.h"
+
 extern GameEngine::Application* GameEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hello");
+	GameEngine::Log::Init();
+	GE_CORE_ERROR("Initialized Log...");
+	int a = 5;
+	GE_INFO("Here's some info... Var={0}", a);
+
+
 	auto app = GameEngine::CreateApplication();
 	app->Run();
 
---
+++ GameEngine/src/GameEngine/Log.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace GameEngine {
+
+	class GAMEENGINE_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros for message logging.
+// (...) means any number of arguments and the (__VA_ARGS__) just passes those arguments
+// from the base call to that function call.
+#define GE_CORE_TRACE(...)   ::GameEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define GE_CORE_INFO(...)    ::GameEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define GE_CORE_WARN(...)    ::GameEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define GE_CORE_ERROR(...)   ::GameEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define GE_CORE_FATAL(...)   ::GameEngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client lg macros
+#define GE_TRACE(...)   ::GameEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define GE_INFO(...)    ::GameEngine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define GE_WARN(...)    ::GameEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define GE_ERROR(...)   ::GameEngine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define GE_FATAL(...)   ::GameEngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
+// If distribution build
+#define GE_CORE_INFO
